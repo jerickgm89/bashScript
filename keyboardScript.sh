8fbc81b38620b7fbf513b9a28b2f35c3817c9237
JErickDev <jerickgm89@gmail.com>
2023-12-13 09:17:12 -0500

    Agregando polychromatic-cli para cambiar los colores del teclado

--- keyboardScript.sh
+++ keyboardScript.sh
@@ -2,11 +2,11 @@
 
 keyboardLaptop=$(xinput | grep "AT Translated Set 2 keyboard" | cut -d'=' -f2 | cut -d'[' -f1)
 keyboardHunstman=$(xinput | grep "Razer Razer Huntsman Mini" | cut -d'=' -f2 | cut -d'[' -f1)
-echo "El teclado del laptop es: $keyboardLaptop"
 
 if [ -n "$keyboardHunstman" ]; then
     echo "El teclado Hunstman esta conectado"
     xinput disable $keyboardLaptop
+    polychromatic-cli -e JErickDev
 else
     echo "El teclado Hunstman no esta conectado"
     xinput enable $keyboardLaptop
