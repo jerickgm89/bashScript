3ce9563663ce83b085a1a2d66f6343e919ae7e47
JErickDev <jerickgm89@gmail.com>
2023-12-15 23:21:47 -0500

    ADD pruebas internas

--- keyboardScript.sh
+++ keyboardScript.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 keyboardLaptop=$(xinput | grep "AT Translated Set 2 keyboard" | cut -d'=' -f2 | cut -d'[' -f1)
-keyboardHunstman=$(xinput | grep "Razer Razer Huntsman Mini" | cut -d'=' -f2 | cut -d'[' -f1)
+keyboardHunstman=$(xinput | grep "Razer Razer Huntsman Mini" | cut -d'=' -f2 | cut -d'[' -f1 | head -n 1)
+
 
 if [ -n "$keyboardHunstman" ]; then
-    echo "El teclado Hunstman esta conectado"
     xinput disable $keyboardLaptop
     polychromatic-cli -e JErickDev
 else
-    echo "El teclado Hunstman no esta conectado"
     xinput enable $keyboardLaptop
 fi
+
